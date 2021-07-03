241f83d1216c4ba9b5e11f8de1acc936cd4a5d3c
ibrahimcicek <ibrahim.cicek17@gmail.com>
2021-07-03 12:43:24 +0300

    Update main.cpp

--- Sorting_Algotithms/main.cpp
+++ Sorting_Algotithms/main.cpp
@@ -1,8 +1,45 @@
 #include<iostream>
 
 using namespace std;
+#define MAXI 50
+
+
+void bubbleshort(int dizi[], int boyut)
+{
+	for(int i = 0; i<boyut; i++)
+	{
+		for (int j = 1; j <boyut - i; j++)
+		{
+			if (dizi[j-1] > dizi[j])
+			{
+				int dummy = dizi[j];
+				dizi[j] = dizi[j-1];
+				dizi[j-1] = dummy;
+			}
+		}
+	}
+}
+
 
 int main()
 {
+	int boyut;
+	cout << "dizinin boyutunu giriniz: ";
+	cin >> boyut;
+	int sayi_dizisi[MAXI];
+	cout << endl;
+	for (int i = 0; i < boyut; i++)
+	{
+		cin >> sayi_dizisi[i];
+	}
+	cout << endl;
+	bubbleshort(sayi_dizisi, boyut);
+	for (int i = 0; i <boyut; i++)
+	{
+		cout << sayi_dizisi[i] << ",";
+	}
+	
+	
+
 
 }
